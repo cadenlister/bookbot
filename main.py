eaf998a4a73dfd64a16ad553b67609747b9fd498
cadenlister <cadenlister@gmail.com>
2025-05-24 15:19:18 -0500

    D: list sorted correctly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import count_words
 from stats import count_characters
+from stats import word_sort
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -7,11 +9,14 @@ def get_book_text(filepath):
 
 def main():
     word_library = {}
+    sorted_words = []
     book_text = get_book_text("books/frankenstein.txt")
     count_word = count_words(book_text)
     word_library = count_characters(book_text)
-    print (f"{count_word} words found in the document")
-    print (f"{word_library} words found in the document")
+    sorted_words = word_sort(word_library)
+    #print (f"{count_word} words found in the document")
+    #print (f"{word_library} words found in the document")
+    
 
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,25 @@ def count_characters(texts):
         else:
             character_counts[text] = 1
     return (character_counts)
+
+def sorting(dict): #used to act as a key to sort through the dict list
+    return dict["num"]
+
+def word_sort(words): #takes a dictionary as an input and then creates a list of dictionaries
+    dict_list = []
+    name_dict = {}
+    for k,v in words.items():
+        name_dict["char"] = k
+        name_dict["num"] = v
+        dict_list.append(name_dict)
+        name_dict={}
+    print(dict_list)
+    dict_list.sort(reverse=True, key=sorting)
+    print (dict_list)
+    return dict_list
+        
+    
+    
+        
+    
+    
